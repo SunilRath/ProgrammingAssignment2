4d5f2bd12d761e3d38816faf0081911157f8702c
unknown <cwr.Sunil.Rath@L-156057645.int.asurion.com>
2014-11-23 15:50:56 -0800

    Inverse Matrix cached V1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This following two functions caches the Inverse of a Matrix 
+## if the value of the matrix has not changed.(Matrix should be square invertible)
 
-## Write a short comment describing this function
+## This function creates a list containing a function to set & get the matrix 
+## and set & get the inverse of a Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## Initialize inverse of the Matrix as NULL
+  inv <- NULL
+  
+  ## Set the value of the x Matrix from a object y of different Environment
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  ## Get function that returns the current value in Matrix x
+  get <- function() x
+  
+  ## Function to Assign inverse of a matrix to inv 
+  setinverse <- function(inverse) inv <<- inverse
+  
+  ## Function to Get the current value of inv
+  getinverse <- function() inv
+  
+  ## Return a list of containing a function to set & get the matrix 
+  ## and set & get the inverse of a Matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function either retrieves the inverse of a matrix x from cache or
+## calculates the inverse of a given matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Get the inverse of the Matrix x
+  inv <- x$getinverse()
+  
+  ## Check if the inverse is cached and return it if present
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## If the inverse of the Matrix is not present, Get the Matrix x
+  data <- x$get()
+  
+  ## Calculate the Inverse of the Matrix using solve
+  inv <- solve(data)
+  
+  ## Set the inverse of the Matrix x into Cache
+  x$setinverse(inv)
+  
+  ## Retrurn the inverse of the Matrix
+  inv
 }
